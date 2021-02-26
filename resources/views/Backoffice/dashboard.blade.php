f47f9e1267ff2b842c7de892490431c712d2724c
vanessa <vanessapcbarros@gmail.com>
2021-02-26 11:32:11 +0100

    create languages folders for sync translations

--- resources/views/Backoffice/dashboard.blade.php
+++ resources/views/Backoffice/dashboard.blade.php
@@ -1,10 +1,29 @@
-<div>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-        <button type="submit">logout</button>
-    </form>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    
-</div>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Caritas Badalona</title>
+</head>
 
-<div><span>{{$user->email}}</span></div>
+<body>
+    <div>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+            <button type="submit">logout</button>
+        </form>
+
+    </div>
+
+    <section>
+        <h2>Datos de Registro</h2>
+        <span>{{$user->email}}</span>
+        <button>{{ __('buttons.update') }}</button>
+
+    </section>
+
+</body>
+
+</html>
