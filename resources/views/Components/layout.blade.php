eaab262885e5c91a9c5aeac338998cb847b3e29b
renejfc <renejfc@gmail.com>
2021-03-01 11:41:19 +0100

    global layout added

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/css/app.css" rel="stylesheet">
+        <title>{{ $title ?? 'Càritas @ Badalona' }}</title>
+    </head>
+    <body>
+      {{ $slot }}
+    </body>
+</html>
