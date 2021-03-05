d079e93cf673bde619a3d43081133cfe0cebcb1c
pilpod <67429595+pilpod@users.noreply.github.com>
2021-03-05 10:15:10 +0100

    user can update profile, name and email in dashboard

--- resources/views/Auth/updateUserProfile.blade.php
+++ resources/views/Auth/updateUserProfile.blade.php
@@ -9,13 +9,21 @@
 </head>
 
 <body>
+
+    @if(session('status') == "profile-information-updated")
+        <div>
+            <h2>Perfil actualizado con éxito</h2>
+        </div>
+        <a href="{{ route('dashboard') }}">Volver</a>
+    @else
+
     <form method="POST" action="{{ route('user-profile-information.update') }}">
         @method("PUT")
         @csrf
         <div class="flex flex-col">
             <label for="name" class="">Nombre</label>
-            <input id="name" type="text" class=" border-gray-200 border-2 p-2 rounded mb-5 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nombre">
-            @error('name')
+            <input id="name" type="text" class=" border-gray-200 border-2 p-2 rounded mb-5 @error('name') is-invalid @enderror" name="name" value="{{ old('name') ?? auth()->user()->name }}" required autocomplete="name" autofocus placeholder="Nombre">
+            @error('name', 'profile-information-updated')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -24,8 +32,8 @@
 
         <div class="flex flex-col">
             <label for="email" class="">Correo Electrónico</label>
-            <input id="email" type="email" class="border-gray-200 border-2 p-2 rounded mb-5 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Correo Electrónico">
-            @error('email')
+            <input id="email" type="email" class="border-gray-200 border-2 p-2 rounded mb-5 @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? auth()->user()->email}}" required autocomplete="email" placeholder="Correo Electrónico">
+            @error('email', 'profile-information-updated')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -33,6 +41,7 @@
         </div>
         <button name="register" id="register" class=" block btn w-1/2 mx-auto bg-red-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded border-b-4 border-red-500" type="submit">Actualizar</button>
     </form>
+    @endif
 </body>
 
 </html>
