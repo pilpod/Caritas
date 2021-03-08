4694c9987713869bd76f25c4b16faf1f61b07a56
G4BR1TRZ <67324204+G4BR1TRZ@users.noreply.github.com>
2021-03-08 15:40:20 +0100

    footer styling

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -3,13 +3,15 @@
   <section class="flex">
 
     <div class="border-2 border-red flex-1 flex flex-col items-center text-center">
-    <br>
-      <x-anchor
-      href="mailto:santjosepbdn@mail.com" 
-      txt="santjosepbdn@mail.com"/>
-      <x-anchor 
-      href="#" 
-      txt="345 Faulconer Drive, Suite 4 - Charlottesville, CA, 12345"/>
+      <br>
+      <div class="flex flex-col items-center">
+        <x-anchor
+        href="mailto:santjosepbdn@mail.com" 
+        txt="santjosepbdn@mail.com"/>
+        <x-anchor 
+        href="#" 
+        txt="345 Faulconer Drive, Suite 4 - Charlottesville, CA, 12345"/>
+      </div>
     </div>
 
     <div class="flex-none">
@@ -30,15 +32,14 @@
 
 
   </section>
- <hr>
-  <section class="flex">
-    <div class="border-4 border-red-400">
-      <ul>
+  <section>
+      <ul class="inline-flex flex-1 justify-between w-5/12 ml-4">
         <li><x-anchor href="#" txt="Avíso legal" /></li>
         <li><x-anchor href="#" txt="Política de privacidad" /></li>
         <li><x-anchor href="#" txt="Política de cookies" /></li>
-        <li><x-anchor href="#" txt="Copyright © 2021 • Cáritas Española" /></li>
       </ul>
-    </div>
+      <ul class="inline-flex flex-1 flex-row-reverse w-6/12">
+      <li><x-anchor href="#" txt="Copyright © 2021 • Cáritas Española" /></li>
+      </ul>
   </section>
 </footer>
--- resources/views/Components/youtubeiframe.blade.php
+++ resources/views/Components/youtubeiframe.blade.php
@@ -1,3 +1,3 @@
 <section >
-    <iframe class="w-11/12 m-auto h-80 rounded " src="https://www.youtube.com/embed/Rqt5B2Ko0Es" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe class="w-full m-auto h-96 " src="https://www.youtube.com/embed/Rqt5B2Ko0Es" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </section>
