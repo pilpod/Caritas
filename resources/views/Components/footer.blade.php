4ebd76ec315a8278cd679cc36e02c64ffb202f19
G4BR1TRZ <67324204+G4BR1TRZ@users.noreply.github.com>
2021-03-08 15:04:17 +0100

    added footer style

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,30 +1,44 @@
 <footer>
  <hr>
-  <section>
-    <div>
-      <x-anchor 
+  <section class="flex">
+
+    <div class="border-2 border-red flex-1 flex flex-col items-center text-center">
+    <br>
+      <x-anchor
       href="mailto:santjosepbdn@mail.com" 
       txt="santjosepbdn@mail.com"/>
       <x-anchor 
       href="#" 
       txt="345 Faulconer Drive, Suite 4 - Charlottesville, CA, 12345"/>
     </div>
-    <x-logo href="#" />
-    <ul>
-      <p>Cáritas</p>
-      <li>Cáritas C</li>
-      <li>Cáritas C </li>
-      <li>Cáritas C</li>
-      <li>Cáritas C</li>
-    </ul>
+
+    <div class="flex-none">
+      <x-logo href="#" />
+    </div>
+
+    <div class="border-2 border-gray-400 flex-1">
+      <ul class="text-center">
+        <p>Cáritas</p>
+        <li>Cáritas A</li>
+        <li>Cáritas B</li>
+        <li>Cáritas C</li>
+        <li>Cáritas D</li>
+      </ul>
+    </div>
+
+
+
+
   </section>
  <hr>
-  <section>
-    <ul>
-      <li><x-anchor href="#" txt="Avíso legal" /></li>
-      <li><x-anchor href="#" txt="Política de privacidad" /></li>
-      <li><x-anchor href="#" txt="Política de cookies" /></li>
-      <li><x-anchor href="#" txt="Copyright © 2021 • Cáritas Española" /></li>
-    </ul>
+  <section class="flex">
+    <div class="border-4 border-red-400">
+      <ul>
+        <li><x-anchor href="#" txt="Avíso legal" /></li>
+        <li><x-anchor href="#" txt="Política de privacidad" /></li>
+        <li><x-anchor href="#" txt="Política de cookies" /></li>
+        <li><x-anchor href="#" txt="Copyright © 2021 • Cáritas Española" /></li>
+      </ul>
+    </div>
   </section>
 </footer>
