838a81024d8b399da73f307caf559d9972088570
renejfc <renejfc@gmail.com>
2021-03-09 10:42:02 +0100

    :construction: Working on mobile styles

--- resources/views/Components/mini-hero.blade.php
+++ resources/views/Components/mini-hero.blade.php
@@ -1,13 +1,13 @@
 @props(['presentation' => 'normal', 'filename', 'alt'])
 
-<div>
+<div class="flex flex-col justify-center w-full">
   @switch($presentation)
     @case('normal')
     {{ $slot }}
-    <img src="{{ asset('storage/img/' . $filename) }}" alt="{{ $alt }}">
+    <img src="{{ asset('storage/img/' . $filename) }}" alt="{{ $alt }}" class="w-36 h-36">
       @break
     @case('reverse')
-    <img src="{{ asset('storage/img/' . $filename) }}" alt="{{ $alt }}">
+    <img src="{{ asset('storage/img/' . $filename) }}" alt="{{ $alt }}" class="w-36 h-36">
     {{ $slot }}
       @break
   @endswitch
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,3 +1,42 @@
 <x-layout class="flex flex-col place-items-center mt-10">
-    <x-separator />
+<x-separator />
+    <x-mini-hero filename="family.svg" alt="">
+        <h1 class="text-h1">Las familias no pueden cubrir sus necesidades</h1>
+    </x-mini-hero>
+    <x-downtick-list title="Trabajo precario y paro">
+        <li>Paro de larga duración</li>
+        <li>Prestaciones públicas insuficientes</li>
+        <li>Salarios bajos</li>
+    </x-downtick-list>
+    <x-downtick-list title="Baja protección social">
+        <li>Prestaciones públicas en descenso.</li>
+        <li>Recortes en educación, sanidad, servicios sociales.</li>
+    </x-downtick-list>
+    <x-downtick-list title="Fragilidad humana">
+        <li>Roturas familiares</li>
+        <li>Sin red relacional (familia, amistades, etc)</li>
+        <li>Enfermedades</li>
+    </x-downtick-list>
+    <x-mini-hero filename="giving.svg" alt="">
+        <h1 class="">El actual modelo se basa en las ayudas en especie</h1>
+    </x-mini-hero>
+<x-separator />
+    <x-mini-hero filename="innovation.svg" alt="">
+        <ul>
+            <li>Potenciar proyectos igualitarios, favorecer la discreción y evitar circuitos para pobres.</li>
+            <li>Mantener los hábitos y las relaciones familiares (cocinar, comer, celebrar, etc).</li>
+            <li>Impulsar proyectos donde las personas participen.</li>
+            <li>Promover el intercambio y la reciprocidad.</li>
+            <li>Dignificar al máximo la atención.</li>
+        </ul>
+    </x-mini-hero>
+    <x-mini-hero filename="bestof.svg" alt="">
+        <ul>
+            <li>Promover la autonomía y el empoderamiento de la persona (modelo no asistencialista)</li>
+            <li>Elegir lo que se come, según gustos personales, culturales o de salud.</li>
+            <li>Evitar el desperdicio y fomentar la sostenibilidad.</li>
+            <li>Mantener el comercio local y de proximidad.</li>
+            <li>Promover la formación y la inserción laboral.</li>
+        </ul>
+    </x-mini-hero>
 </x-layout>
