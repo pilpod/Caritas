977061989a35ebf442789091f767d34e00832712
Alvaro <astaboada7@gmail.com>
2021-03-10 12:49:08 +0100

    minor cosmetics, button is right

--- resources/views/Components/back-to-top.blade.php
+++ resources/views/Components/back-to-top.blade.php
@@ -1,16 +1,13 @@
-<div class="flex w-screen h-14 place-content-center fixed right-6 md:right-4">
-    {{-- <a class="" href="#"> --}}
-    <button type="button" onclick="topFunction()" id="backToTop" title="backToTop" class="fixed bottom-3 border-solid border-white w-10 h-10 rounded-md bg-red hover:bg-red-light ">
+<div class="w-screen h-14 fixed">
+    <button type="button" onclick="topFunction()" id="backToTop" title="backToTop" class="fixed m-4 right-4 bottom-3 border-solid border-white w-10 h-10 rounded-md bg-red hover:bg-red-light ">
     <img class="object-contain" src="{{ asset('storage/img/backToTop.png') }}" alt="BackToTop"/></button>
-    {{-- </a>     --}}
 </div>
 
 @push('script-back-to-top')    
 <script>
-    //Get the button
+
     let buttonBackToTop = document.getElementById("backToTop");
     
-    // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
     
     function scrollFunction() {
@@ -21,7 +18,6 @@
         }
     }
     
-    // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
