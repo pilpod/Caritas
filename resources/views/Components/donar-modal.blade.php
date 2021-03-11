b16347d8e3317484085229d6e5f520416637beda
Alvaro <astaboada7@gmail.com>
2021-03-11 09:22:53 +0100

    donar modal half way through

--- resources/views/Components/donar-modal.blade.php
+++ resources/views/Components/donar-modal.blade.php
@@ -2,45 +2,54 @@
 
 
 <article>
-    <div id="container" class="text-center mx-4 text-mobile-main">
-        <h1 class="text-h1"> Donar</h1>
-        <p>
-            Puedes hacer una donación por transferencia bancaria o con el número de Bizum:
-        
-            {{'Fiare. Banca Ética 
-            ES05 – 1550 – 0001 – 2800 – 0193 – 5626
-            codigo 33432'}}
-        </p>
-        <p>
-            BENEFICIOS FISCALS
-
-            Con tu aportación económica disfrutarás de beneficios fiscales que te permitirán desgravar los donativos en la Declaración de la Renta o Impuesto de Sociedades. Si quieres desgravar envianos al correo
-            {{'caritasbdnsantjosep@gmail.com'}} con los siguientes datos: 
-
-            PARTICULAR: Nombre i Apellido / DNI / dirección postal
-            EMPRESA o ENTITAD: Razón social / CIF / dirección postal          
-
-        </p>
-        <div class="flex flex-col items-center border-2 border-red bg-donar bg-center bg-cover rounded-3xl my-10">
-            <img class="relative left-24 bottom-8" src="{{ asset('storage/img/decoLogoIzq.png')}}" alt="">
-            <p class="m-4">
-                amb 10 € aportaràs llet per una parella durant un mes.
+    <div class="bg-red-light">
+        <a href="#"><h1 class="text-h1 text-right">X</h1></a>
+        <div id="container" class="bg-white-dark text-center mx-4 text-mobile-main rounded-3xl">
+            <h1 class="text-h1 m-4"> Donar</h1>
+            <p class="my-4">
+                Puedes hacer una donación por transferencia bancaria o con el número de Bizum:
+                <br>
+                <br>
+                {{ 'Fiare. Banca Ética 
+                ES05 – 1550 – 0001 – 2800 – 0193 – 5626' }}
+                {{ 'codigo 33432' }}
             </p>
-            <img class="w-20 h-20 m-6" src="{{ asset('storage/img/milk.png')}}" alt="">
-            <p class="m-4">
-                amb 20 € tindran també un plat de llegums, pasta o arròs.
-            </p>  
-            <img class="w-20 h-20 m-6" src="{{ asset('storage/img/pasta.png')}}" alt="">
-            <p class="m-4"> 
-                amb 50 € podran comprar peix i carn per un mes 
+            <p  class="my-4">
+                BENEFICIOS FISCALES
+                <br>
+                <br>
+                Con tu aportación económica disfrutarás de beneficios fiscales que te permitirán desgravar los donativos en la Declaración de la Renta o Impuesto de Sociedades. Si quieres desgravar envianos al correo
+                <x-anchor
+                href="mailto:santjosepbdn@gmail.com" 
+                txt="santjosepbdn@mail.com"/> con los siguientes datos: 
+                <br>
+                PARTICULAR: Nombre i Apellido / DNI / dirección postal
+                EMPRESA 
+                <br>
+                ENTITAD: Razón social / CIF / dirección postal          
+
             </p>
-            <img class="w-32 h-32 m-6" src="{{ asset('storage/img/fish.png')}}" alt="">
-            <p class="m-4">
-                amb més € hi afegiràs productes de neteja
-            </p>
-            <img class="w-20 h-20 m-6" src="{{ asset('storage/img/soap.svg')}}" alt="">
-            <img class="relative right-20 top-8 z-2" src="{{ asset('storage/img/decoLogoDrch.png')}}" alt="">
+            <div class="flex flex-col items-center border-2 border-red bg-donar bg-center bg-cover rounded-3xl my-10">
+                <img class="relative left-20 bottom-16" src="{{ asset('storage/img/decoLogoIzq.png')}}" alt="">
+                <p class="m-2">
+                    con 10 € aportarás leche para una pareja durante un mes.
+                </p>
+                <img class="w-20 h-20 m-4" src="{{ asset('storage/img/milk.png')}}" alt="">
+                <p class="m-2">
+                    con 20 € tendrán también un plato de legumbres, pasta o arróz.
+                </p>  
+                <img class="w-20 h-20 m-4" src="{{ asset('storage/img/pasta.png')}}" alt="">
+                <p class="m-2"> 
+                    con 50 € podrán comprar pescado y carne para un mes 
+                </p>
+                <img class="w-32 h-32 m-4" src="{{ asset('storage/img/fish.png')}}" alt="">
+                <p class="m-2">
+                    amb més € hi afegiràs productes de neteja
+                </p>
+                <img class="w-20 h-20 m-6" src="{{ asset('storage/img/soap.svg')}}" alt="">
+                <img class="relative right-24 top-10 w-24 z-2" src="{{ asset('storage/img/decoLogoDrch.png')}}" alt="">
+            </div>
         </div>
-
+      
     </div>
 </article>
