d447629992fa3ad637b3ef3104ad28ee45f57804
Alvaro <astaboada7@gmail.com>
2021-03-11 19:37:47 +0100

    security commit bevor try new component philosophy

--- resources/views/Components/logo.blade.php
+++ resources/views/Components/logo.blade.php
@@ -2,6 +2,6 @@
 
 <div class="flex items-center w-24 h-24 rounded-md">
   <a href="{{ $href ?? $href }}" target="_blank">
-    <img src="{{ asset('storage/img/logoSantJosep.jpeg') }}" alt="Caritas Logo"/>
+    <img src="{{ asset('img/logo/'. $profile->logo) }}" alt="Caritas Logo"/>
   </a>
 </div> 
