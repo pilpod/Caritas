8549ea9f013b9df818dbe7968907c339110342d0
vanessa <vanessapcbarros@gmail.com>
2021-03-12 17:52:56 +0100

    update image about working and change Components folders name to components

--- resources/views/Backoffice/about.blade.php
+++ resources/views/Backoffice/about.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <div>
-        <form method="POST" action="{{route('about.updateImage')}}" enctype="multipart/form-data">
-        @method('PUT');
-        @crsf
+        <form method="POST" action="{{route('about.updateImage', $sectionId)}}" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
             <label for="section_image">Imagen de Sección</label>
             <input type="file" name="section_image" @errror('section_image') is-invalid placeholder="Seleccionar image">
             @error('section_image')
