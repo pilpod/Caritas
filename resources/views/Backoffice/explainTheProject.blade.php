16739d0575ccbf95a1fcec7e3cb84b31ff003891
vanessa <vanessapcbarros@gmail.com>
2021-03-16 10:11:00 +0100

    change the explain the project view and add link on dashboard

--- resources/views/Backoffice/explainTheProject.blade.php
+++ resources/views/Backoffice/explainTheProject.blade.php
@@ -1,3 +1,20 @@
-<div>
-    herer yu explain the project thingy
-</div>
+<x-backoffice-layout>
+    <div>
+        <form method="POST" action="{{route('explainTheProject.updateImage', $sectionId)}}" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+            <label for="section_image">Imagen de Sección</label>
+            <input type="file" name="section_image" @error('section_image') is-invalid @enderror placeholder="Seleccionar image">
+            @error('section_image')
+            <div>{{$message}}</div>
+            @enderror
+
+            <x-backoffice-button txt="Cargar"/> 
+        </form>
+    </div>
+    <div>
+        <form action="POST">
+        </form>
+    </div>
+
+</x-backoffice-layout>
