9bd0c55ad549f5c5c3c67bcfeeaf7deccd81fcda
Alvaro <astaboada7@gmail.com>
2021-03-17 20:01:30 +0100

    all text variables from Db decoded

--- resources/views/components/modals/who-we-are-component.blade.php
+++ resources/views/components/modals/who-we-are-component.blade.php
@@ -4,7 +4,7 @@
     <img class="h-150px  md:h-450px rounded"src="{{ asset('storage/section/' . $aboutImg ) }}" alt="Caritas Logo"/>
 </div>
 <p class="text-ui-tiny mx-6 my-4">
-    @if(app()->getLocale() == 'es') {{ $aboutEs }}
-    @else {{ $aboutCat }}
+    @if(app()->getLocale() == 'es') {!! html_entity_decode($aboutEs) !!}
+    @else {!! html_entity_decode($aboutCat) !!}
     @endif</p>
 <x-anchor class="mx-6 my-4" href="#" txt="LINK POR PONER"/>
