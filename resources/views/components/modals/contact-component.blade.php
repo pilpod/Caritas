17e941908aed548a048b35da5c0e203643a97690
Alvaro <astaboada7@gmail.com>
2021-03-18 09:44:04 +0100

    mvp-wise contact modal done

---
+++ resources/views/components/modals/contact-component.blade.php
@@ -0,0 +1,16 @@
+
+<div id="container" class="bg-white-dark text-black text-center mx-4 text-mobile-main rounded-3xl">
+    <h1 class="text-h1 m-4"> {{__('contact-title')}}</h1>
+    
+    <div class="flex flex-col items-center border-2 border-red bg-donar bg-center bg-cover rounded-3xl my-10 lg:text-d-ui-main ">
+        <p class="my-4">
+            {{__('contact-text')}} 
+            <br>
+            <br>
+        </p>
+        <p class="text-d-h4">
+            {{ 'santjosepbdn@mail.com' }} <br>
+        </p>
+        
+    </div>
+</div>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,7 +7,7 @@
     <x-button txt="{{ __('nav-quienes-somos') }}" nav-modal><x-modals.who-we-are-component /></x-button>
     <x-logo-component href="https://www.santjosepbadalona.cat/" class="nav__logo" />
     <x-button txt="{{ __('nav-que-puedes-hacer-tu') }}" nav-modal><x-modals.what-can-you-do-component /></x-button>
-    <x-button txt="{{ __('nav-contacto') }}" nav-modal>Content</x-button>
+    <x-button txt="{{ __('nav-contacto') }}" nav-modal><x-modals.contact-component /></x-button>
     <x-burguer-menu class="lg:hidden"/>
 </nav>
 
