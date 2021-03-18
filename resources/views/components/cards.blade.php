5ea1e57d29109b0b55435073fa2748bcff3d5064
Alvaro <astaboada7@gmail.com>
2021-03-18 07:38:21 +0100

    modals windows uppercase bug fixed

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -3,8 +3,8 @@
 ​
 <div class="flex flex-row w-11/12 my-5 border-grey-light border p-2 rounded">
     <img class="w-1/6 p-2" src="{{ asset('storage/section/' . $filename) }}" alt="">
-    <div class="flex flex-col">
-      <p class="font-bold">{{ $titleCard }}</p>
+    <div class="flex flex-col normal-case">
+      <p class="font-sans">{{ $titleCard }}</p>
       {{ $slot }}
       <div class="flex flex-row-reverse">
       <button type="button" class="text-white text-mobile-tiny bg-gradient-to-b from-red-lighter via-red-light to-red border-red object-center my-6 p-2.5 rounded">{{$button ?? ''}}</button>
--- resources/views/components/modals/who-we-are-component.blade.php
+++ resources/views/components/modals/who-we-are-component.blade.php
@@ -1,9 +1,9 @@
 <h2 class="text-h3 flex text-center justify-center mb-6">QUIENES SOMOS</h2>
-<div class="flex justify-center">
+<div class="flex justify-center ">
  
     <img class="h-150px  md:h-450px rounded"src="{{ asset('storage/section/' . $aboutImg ) }}" alt="Caritas Logo"/>
 </div>
-<p class="text-ui-tiny mx-6 my-4">
+<p class="text-ui-tiny mx-6 my-4 normal-case">
     @if(app()->getLocale() == 'es') {!! html_entity_decode($aboutEs) !!}
     @else {!! html_entity_decode($aboutCat) !!}
     @endif</p>
