36042326efcf576238878109cd4bd178e3f24c20
Alvaro <astaboada7@gmail.com>
2021-03-18 07:46:47 +0100

    logo link bug fixed

--- resources/views/components/logo-component.blade.php
+++ resources/views/components/logo-component.blade.php
@@ -1,5 +1,5 @@
 @props(['href', 'txt', 'container'])
 
-<a href="{{ $href ?? $href }}" target="_blank" class="place-self-center {{ $container ?? '' }}">
+<a href="{{ $href ?? 'https://www.santjosepbadalona.cat/' }}" target="_blank" class="place-self-center {{ $container ?? '' }}">
     <img src="{{ asset('storage/logo/' . $profile->logo) }}" alt="Logo" {{ $attributes }} />
 </a>
