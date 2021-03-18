d542f53b9b11dcc619c422bf5b886655cee42570
Alvaro <astaboada7@gmail.com>
2021-03-18 13:58:21 +0100

    donar modal styles bettered

--- resources/views/components/modals/donation-component.blade.php
+++ resources/views/components/modals/donation-component.blade.php
@@ -1,16 +1,21 @@
 <a href="#"><h1 class="text-h1 text-right mr-6">x</h1></a>
-<div id="container" class="bg-white-dark text-black text-center mx-4 text-mobile-main rounded-3xl">
+<div id="container" class="bg-white-dark text-black text-center mx-4 text-body lg:text-d-main rounded-3xl">
     <h1 class="text-h1 m-4"> {{__('donation-title')}}</h1>
-    <p class="my-4">
-        {{__('donation-account')}} 
-        <br>
-        <br>
-        {{ $profile->bankAccount }} <br>
-        {{__('donation-bizum')}}<br>
-        {{ $profile->bizum }}
+    <p class="my-4 text-ui-main lg:text-d-ui-main">
+        {{__('donation-account')}} <br>
     </p>
-    <p  class="my-4">
-        {{__('donation-tax-relief-title')}}
+    <h4 class="text-ui-main lg:text-d-ui-main">
+        {{ $profile->bankAccount }} <br>
+    </h4>
+    <p>
+        {{__('donation-bizum')}}<br>
+    </p>
+    <h4 class="text-ui-main lg:text-d-ui-main">
+        {{ $profile->bizum }}
+    </h4>
+
+    <h2 class="text-h4 lg:text-d-h4 mt-9">{{__('donation-tax-relief-title')}}</h2>
+    <p  class="mb-4">
         <br>
         <br>
         {{__('donation-tax-relief-text')}}
@@ -24,7 +29,7 @@
 
     </p>
     <div class="flex flex-col items-center border-2 border-red bg-donar bg-center bg-cover rounded-3xl my-10">
-        <img class="relative left-20 bottom-16" src="{{ asset('storage/img/decoLogoIzq.png')}}" alt="">
+       
         <p class="m-2">
             {{__('donation-10')}}
         </p>
@@ -41,6 +46,5 @@
             {{__('donation-+50')}}
         </p>
         <img class="w-20 h-20 m-6" src="{{ asset('storage/img/soap.svg')}}" alt="">
-        <img class="relative right-24 top-10 w-24 z-2" src="{{ asset('storage/img/decoLogoDrch.png')}}" alt="">
     </div>
 </div>
